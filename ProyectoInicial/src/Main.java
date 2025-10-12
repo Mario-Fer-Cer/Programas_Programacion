0bf135029ccb1b254884b0643927350dd4d7ee4a
Mario Fernandez Cervera <atlas_karonte18@icloud.com>
2025-10-12 02:03:23 +0200

    Primer Programa
    Se crea el primer programa, el inicio de programacion.

---
+++ ProyectoInicial/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hola, este es el principio de tu carrera");
+        System.out.print("Este print aunque no incluye un salto de linea con ln tiene el salto de linea\n");
+        System.out.println("16161616 Codigo no man's sky");
+    }
+}
